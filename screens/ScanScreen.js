d2daf925d725be3cb4bfe6e050685543eb943e1e
GinusHR <145678936+GinusHR@users.noreply.github.com>
2025-06-16 15:38:34 +0200

    Setup list page

--- screens/ScanScreen.js
+++ screens/ScanScreen.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet, Text, FlatList} from 'react-native';
 
-export default function ScanScreen() {
+
+const [items, setItems] = useState([])
+
+
+
+export default function ScanScreen({list}) {
+    useEffect(() => {
+        (async () => {
+                try {
+                    console.log("Collecting info")
+                    setItems(list);
+                    console.log("List found")
+                } catch (error) {
+                    console.log("AAAAAAAAAAAAAAAAAAgh", error)
+                }
+        })();
+    }, []);
     return (
         <View>
             <Text>Scan</Text>
+            <FlatList data={items} renderItem={}></FlatList>
         </View>
     );
 }
