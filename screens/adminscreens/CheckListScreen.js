92148fd2cab7c7fdb4983cd406fd2a7c1284b8ea
Vincent Benders <74290916+VincentBenders@users.noreply.github.com>
2025-06-18 09:45:02 +0200

    getting items from list

--- screens/adminscreens/CheckListScreen.js
+++ screens/adminscreens/CheckListScreen.js
@@ -1,6 +1,8 @@
 import { useRoute } from "@react-navigation/native";
 import { CameraView } from "expo-camera";
-import { Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Platform, SafeAreaView, StatusBar, StyleSheet, Text, TextInput, View } from "react-native";
+import { getList } from "../../database";
 
 
 export default function CheckListScreen({navigation}) {
@@ -9,13 +11,48 @@ export default function CheckListScreen({navigation}) {
       const code = route.params?.code
     
     // get list by qrcode from the camerascreen page
+    const list = getList(code)
+    console.log('====================================');
+    console.log(list);
+    console.log('====================================');
+    // count bottles/cans per price (0.25, 0.20, 0.15, 0.10)
+  const [number, onChangeNumber] = React.useState('');
 
     return (
         <SafeAreaView style={styleSheet.container}>
-            {/* show list of items */}
         <View>
-            <Text>QR totaal</Text>
-
+            <Text>totaal 0,25 flessen</Text>
+            <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="useless placeholder"
+            keyboardType="numeric"
+            />
+             <Text>totaal 0,20 flessen</Text>
+            <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="useless placeholder"
+            keyboardType="numeric"
+            />
+             <Text>totaal 0,15 flessen</Text>
+            <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="useless placeholder"
+            keyboardType="numeric"
+            />
+             <Text>totaal 0,10 flessen</Text>
+            <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="useless placeholder"
+            keyboardType="numeric"
+            />
         </View>
             
 
